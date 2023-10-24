afa671f86a4cb950c5fc4e44fc844151b35f4022
Your Name <gfc.net@gmail.com>
2023-10-23 21:27:45 -0300

    Project created

---
+++ backend/src/main/java/com/geovanni/geomoney/GeomoneyApplication.java
@@ -0,0 +1,13 @@
+package com.geovanni.geomoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeomoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeomoneyApplication.class, args);
+	}
+
+}
